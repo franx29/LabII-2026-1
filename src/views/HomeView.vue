33b5c47187b4505756253f4116be68657f7443f1
Fabian Camacaro <facpfacp0@gmail.com>
2026-05-08 16:23:46 -0400

    feat: Integracion de componentes a Homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
   <PublicLayout>
-    <!-- Vista de inicio público -->
+    
+    <ObjetivosSection />
+    <ServiciosSection />
+    <ContactoSection />
+
   </PublicLayout>
 </template>
 
 <script setup>
 import PublicLayout from '@/layouts/PublicLayout.vue'
+import ObjetivosSection from '@/components/ObjetivosSection.vue'
+import ServiciosSection from '@/components/ServiciosSection.vue'
+import ContactoSection from '@/components/ContactoSection.vue'
 </script>
