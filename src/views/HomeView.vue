929e946c44fe67095350151281289e07993f2164
Frankriko <newtonx29@gmail.com>
2026-05-12 19:14:38 -0400

    Seccion Header lista y responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
-  <PublicLayout>
-    <!-- Vista de inicio público -->
-  </PublicLayout>
+  <main class="home-page">
+    <HomeHeader />
+  </main>
 </template>
 
 <script setup>
-import PublicLayout from '@/layouts/PublicLayout.vue'
 import HomeHeader from '@/components/HomeHeader.vue'
-import HomeHero from '@/components/HomeHero.vue'
-import HomeBenefitsFlip from '@/components/HomeBenefitsFlip.vue'
-
 </script>
+
+<style scoped>
+html {
+  scroll-behavior: smooth;
+}
+.home-page {
+  background: #f7f9f9;
+  min-height: 100vh;
+}
+</style>
