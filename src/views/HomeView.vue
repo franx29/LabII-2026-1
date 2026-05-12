fc5936a7762884ab6c438765671c6727d8d4c5ee
Frankriko <newtonx29@gmail.com>
2026-05-12 18:29:11 -0400

    archivos base para secciones de Beneficios, banner principal y header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,4 +6,8 @@
 
 <script setup>
 import PublicLayout from '@/layouts/PublicLayout.vue'
+import HomeHeader from '@/components/HomeHeader.vue'
+import HomeHero from '@/components/HomeHero.vue'
+import HomeBenefitsFlip from '@/components/HomeBenefitsFlip.vue'
+
 </script>
