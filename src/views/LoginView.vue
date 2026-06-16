de2a92e6b2bbcb2ebf18866cf3a03ba8de9467ea
ovelio <ovelio55@gmail.com>
2026-06-16 13:59:09 -0400

    -Se agrega el componente Ingreso y la vista LoginView -La vista LoginView compuesta por el Header y el Ingreso. -Se ajusta el Router configurar la ruta de LoginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <PublicLayout>
+
+    <HomeHeader />
+    <Ingreso />
+
+  </PublicLayout>
+</template>
+
+<script setup>
+    import PublicLayout from '@/layouts/PublicLayout.vue'
+
+    import HomeHeader from '@/components/HomeHeader.vue'
+    import Ingreso from '@/components/Ingreso.vue'
+
+</script>
+
+<style scoped>
+html {
+  scroll-behavior: smooth;
+}
+</style>
